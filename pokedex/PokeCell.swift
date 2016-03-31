d4ea42403a6d0b9bb5fee480aaf00c8bba1e3221
rebrooks6 <BrooksFamily@Kaylas-MacBook-Pro-3.local>
2016-03-31 15:42:09 -0500

    added collection vieview and custom PokeCell class

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,23 @@
+//
+//  PokeCell.swift
+//  
+//
+//  Created by Kayla Brooks on 3/31/16.
+//
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
